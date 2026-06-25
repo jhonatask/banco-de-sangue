f140600b945e85b36c94fd25046d82d1649b5a5b
Jhonata Nazareno C Cirqueira <jhonatask@gmail.com>
2026-06-25 13:14:42 -0300

    Base do projeto, adicinando dependencias

---
+++ src/main/java/br/com/jnctecnologia/bancodesangue/BancodesangueApplication.java
@@ -0,0 +1,13 @@
+package br.com.jnctecnologia.bancodesangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodesangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodesangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jnctecnologia/bancodesangue/BancodesangueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jnctecnologia.bancodesangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodesangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
