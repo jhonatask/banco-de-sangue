b8efeff46916e02487d754f49005eb37bd3356b5
Jhonata Nazareno C Cirqueira <jhonatask@gmail.com>
2026-06-26 15:59:57 -0300

    adiciona swagger e libera rotas no security

--- src/main/java/br/com/jnctecnologia/bancodesangue/config/SecurityConfig.java
+++ src/main/java/br/com/jnctecnologia/bancodesangue/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
